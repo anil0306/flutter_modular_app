1b20ece007f76ce171202025aa086b77949a67c5
Anil Kumar <anilkumar@Anils-MacBook-Air.local>
2025-12-14 23:39:37 +0530

    added error handling

--- features/auth/login/lib/domain/repository/login_repository.dart
+++ features/auth/login/lib/domain/repository/login_repository.dart
@@ -1,8 +1,8 @@
 import 'package:dartz/dartz.dart';
 import 'package:domain/model/failure.dart';
 import 'package:login/data/request/login_request.dart';
-import 'package:login/data/response/login_response.dart';
+import 'package:login/domain/model/login_model.dart';
 
 abstract class LoginRepository {
-  Future<Either<Failure, LoginResponse>> login(LoginRequest loginRequest);
+  Future<Either<Failure, LoginModel>> login(LoginRequest loginRequest);
 }
